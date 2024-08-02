dcc50025ced82cd43a42dc8f895f7bb9e34aca73
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-02 17:19:22 +0600

    news page implement

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Button, Card } from 'react-bootstrap';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useLoaderData, useParams } from 'react-router-dom';
+import EditorsInsights from '../EditorsInsights/EditorsInsights';
 
 const News = () => {
-    const {id} = useParams();
+    const news = useLoaderData();
+    const {image_url, _id, title, details, category_id} = news;
     return (
         <div>
-            <h2>This is News: {id}</h2>
+            <Card style={{padding: '30px'}}>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title style={{fontSize: '25px', fontWeight:'bold'}}>{title}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Button className='mt-4' variant="danger">
+                        <Link className='text-decoration-none text-light fw-bold' to={`/category/${category_id}`}><FaArrowLeft />  All news in this category</Link>
+                    </Button>
+                </Card.Body>
+            </Card>
+            <EditorsInsights></EditorsInsights>
         </div>
     );
 };
